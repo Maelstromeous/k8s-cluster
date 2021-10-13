444bbfd4c3a7ad5b5f8b0367bf98296b1c734ceb
Matt Cavanagh <maelstrome26@gmail.com>
2021-10-13 21:34:31 +0100

    Massively fleshed out longhorn process and further setup steps

--- services/longhorn.sh
+++ services/longhorn.sh
@@ -6,13 +6,3 @@
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo apt-get install -y jq open-iscsi nfs-common
-
-# Get GPG key for k8s google repo
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
-sudo apt-get update
-sudo apt-get install -y kubectl
-
-curl -sSfL https://raw.githubusercontent.com/longhorn/longhorn/v1.1.0/scripts/environment_check.sh | bash

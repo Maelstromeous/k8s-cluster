20d8791bbb7c878fe55f030c6da01685f908f3f2
Matt Cavanagh <maelstrome26@gmail.com>
2021-10-13 20:37:04 +0100

    Change longhorn install to be apt method

--- services/longhorn.sh
+++ services/longhorn.sh
@@ -2,9 +2,17 @@
 # Sets up Longhorn disk storage provider.
 # Usage: ./longhorn.sh
 
-# Install kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-
+# Install Deps
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo apt-get install -y jq open-iscsi nfs-common
+
+# Get GPG key for k8s google repo
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubectl
 
 curl -sSfL https://raw.githubusercontent.com/longhorn/longhorn/v1.1.0/scripts/environment_check.sh | bash

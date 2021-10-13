fe472e5f51784f7cbb0c248c887d5cb5fb35509c
Matt Cavanagh <maelstrome26@gmail.com>
2021-10-13 19:50:44 +0100

    Fixed first setup command lacking a sudo

--- firstSetup.sh
+++ firstSetup.sh
@@ -9,6 +9,6 @@ sudo usermod -aG docker $USER
 sudo newgrp docker
 
 # And update the OS for good measure
-sudo apt update && apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 
 echo "Required setup steps complete. If you wish to run docker commands, please re-source or execute 'bash' to re-source that way."
